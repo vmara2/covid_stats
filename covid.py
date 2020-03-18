c53baf0c7516415b1c4449cd59503a819e3ae4ee
vmara2 <valentin.mara11@gmail.com>
2020-03-18 03:06:09 -0500

    Added Illinois grabbing

--- covid.py
+++ covid.py
@@ -16,6 +16,7 @@ soup2 = BeautifulSoup(r2.text, 'html.parser')
 
 covid_stats = []
 
+# gets stats for world
 for header in soup.find_all('h1'):
     if "Coronavirus" in str(header):
         number = header.find_next_sibling()
@@ -24,6 +25,7 @@ for header in soup.find_all('h1'):
         number = header.find_next_sibling()
         covid_stats.append(str(number.get_text()).replace(" ", ""))
 
+# gets stats for US
 for header in soup2.find_all('h1'):
     if "Coronavirus" in str(header):
         number = header.find_next_sibling()
@@ -32,7 +34,20 @@ for header in soup2.find_all('h1'):
         number = header.find_next_sibling()
         covid_stats.append(str(number.get_text()).replace(" ", ""))
 
+# collecting table data for Illinois
+state_table = soup2.table
+table_rows = state_table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    if not row: # incase an array is empty so we don't access it in the next elif
+        continue
+    elif " Illinois " in row[0]:
+        covid_stats.append(str(row[1].replace(" ","")))
+        covid_stats.append(str(row[3].replace(" ","")))
 
 for elem in covid_stats:
     print(elem)
 
+:q
